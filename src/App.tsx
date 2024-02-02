b3ccd95b71b727d63dc004e11a7c22d1985dfbe4
renan-fs-sousa <renan.fs.sousa@gmail.com>
2024-02-02 13:22:58 -0300

    conversão do projeto para styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
+import GlobalStyle, { Container } from './styles'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
-
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
